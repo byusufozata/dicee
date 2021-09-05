2e440c5d551b1a63cf237a965035578f8211e121
YUSUF ÖZATA <byusufozata@gmail.com>
2021-09-05 23:05:32 +0300

    dice

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImgSource = "images/" + randomDiceImage ;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImgSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 ) + 1 ;
+var randomImgSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomImgSource2);
+
+if ( randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "💃🏻Player 1 Wins!"
+}
+else if (randomNumber1< randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!💃🏻"
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw"
+};
